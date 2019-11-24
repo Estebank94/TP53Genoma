f6a8d44ac33f28392933b91f0440e4472b6ab0a3
Esteban Kramer <estebankramer@gmail.com>
2019-11-24 20:39:38 -0300

    Ejercicio 4 y remap output

--- ej4.rb
+++ ej4.rb
@@ -1,14 +1,21 @@
 #!/usr/bin/env ruby
 
-require 'bio'
-
-if ARGV.length != 1
-  puts 'Invalid params: 1) In file path'
+if ARGV.length != 2
+  puts 'Invalid params: 1) In file path of BLAST 2) Search Pattern'
   exit
 end
 
-File.open(ARGV[0], 'w') do |f|
-  f.puts Bio::EMBOSS.run('water',
-                          '-asequence', 'swissprot:slpi_human',
-                          '-bsequence', 'swissprot:slpi_mouse')
-end
+file=ARGV[0]
+f = File.open(file, "r")
+f.each_line { |line|
+  hitList = []
+  auxHit = ""
+  if line.include? 'Hit'
+      auxHit = line
+  elsif line.downcase.include? ARGV[1].downcase
+      puts line
+      hitList << auxHit
+  end
+  puts hitList
+}
+f.close
